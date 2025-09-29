1aa8dee0a6cda10cf5ed3b9b9a35f8b7073fb908
James Wang <wang.james.j@gmail.com>
2025-09-29 16:08:01 -0700

    changed group management

--- main.py
+++ main.py
@@ -7,20 +7,24 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    #basic setup
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
-    updatable = pygame.sprite.Group()
-    drawable = pygame.sprite.Group()
     dt = 0
 
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    # Player.containers = (updatable, drawable)
-    updatable.add(player)
-    drawable.add(player)
+    #managing groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
 
-    print(updatable)
-    print(drawable)
+    #instantiating instances
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    # updatable.add(player)
+    # drawable.add(player)
+
+    # print(updatable)
+    # print(drawable)
 
     while True:
         for event in pygame.event.get():
